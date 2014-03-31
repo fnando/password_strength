b6593c67d683cfb423719e1e745d2d855d1293c5
Nando Vieira <fnando.vieira@gmail.com>
2014-03-31 13:38:10 -0300

    Use bundler.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,9 @@
-$KCODE = "utf8" if RUBY_VERSION < "1.9"
-
+require "bundler/setup"
 require "test/unit"
 require "ostruct"
 require "active_record"
+
+I18n.enforce_available_locales = false
 
 Rails = OpenStruct.new(:version => ActiveRecord::VERSION::STRING)
 require "password_strength"
