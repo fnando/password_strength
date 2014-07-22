9fb5804e2be996cfff3bbe589cd2b90df01b594c
Nando Vieira <fnando.vieira@gmail.com>
2014-07-22 13:36:42 -0300

    Remove unused constant.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,8 +5,6 @@ require "ostruct"
 require "active_record"
 
 I18n.enforce_available_locales = false
-
-Rails = OpenStruct.new(:version => ActiveRecord::VERSION::STRING)
 require "password_strength"
 
 ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
