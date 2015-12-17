de7c18722cad571aa9028bf81bd03153a4efacbf
Nando Vieira <fnando.vieira@gmail.com>
2015-12-17 13:15:24 -0200

    Fix issue with older minitest.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,17 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../../Gemfile", __FILE__)
 require "bundler/setup"
-require "minitest/utils"
+
 require "minitest/autorun"
+
+if defined?(Minitest::Test)
+  begin
+    require "minitest/utils"
+  rescue LoadError
+  end
+else
+  Minitest::Test = MiniTest::Unit::TestCase
+end
+
 require "ostruct"
 require "active_model"
 require "active_support/all"
