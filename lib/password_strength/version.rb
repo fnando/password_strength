c6fbe8d4d043d15f81c31c6faac0e70830a80ed9
Nando Vieira <fnando.vieira@gmail.com>
2015-12-17 13:19:09 -0200

    Bump up version.

--- lib/password_strength/version.rb
+++ lib/password_strength/version.rb
@@ -1,8 +1,8 @@
 module PasswordStrength
   module Version # :nodoc: all
     MAJOR = 1
-    MINOR = 0
-    PATCH = 2
+    MINOR = 1
+    PATCH = 0
     STRING = "#{MAJOR}.#{MINOR}.#{PATCH}"
   end
 end
