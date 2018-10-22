3755b27b8f124c24395ac46e60bf766d84b971a3
Cian McElhinney <cianmce@gmail.com>
2018-10-22 22:17:19 +0100

    add version.rb back

--- lib/password_strength/version.rb
+++ lib/password_strength/version.rb
@@ -1,8 +1,8 @@
-module PasswordStrength 
-  module Version # :nodoc: all  
-    MAJOR = 1 
-    MINOR = 1 
-    PATCH = 5 
-    STRING = "#{MAJOR}.#{MINOR}.#{PATCH}" 
-  end 
+module PasswordStrength
+  module Version # :nodoc: all
+    MAJOR = 1
+    MINOR = 1
+    PATCH = 5
+    STRING = "#{MAJOR}.#{MINOR}.#{PATCH}"
+  end
 end
